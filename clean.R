57a0316cb55136e5af375ddbf03617906af339ea
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:27:05 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -5,4 +5,5 @@ dfStates <<- read.csv("scprc_est_2017_18_pop_res.csv")
   dfStates <<- dfStates[-9,]
   dfStates <<- dfStates[,-1:-4]
   colnames(dfStates) <<- c("stateName","population","popOver18","percentOver18")
+  dfStates
   
