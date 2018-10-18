808e0725f1ad2d96c79fc5fc7bebd14488e0c182
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:27:29 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,5 +1,5 @@
 
-dfStates <<- read.csv("scprc_est_2017_18_pop_res.csv") 
+dfStates <- read.csv("scprc_est_2017_18_pop_res.csv") 
   dfStates <<- dfStates[-1,]
   dfStates <<- dfStates[-52,]
   dfStates <<- dfStates[-9,]
