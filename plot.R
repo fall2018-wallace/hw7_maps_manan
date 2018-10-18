f399f4e60b1a094c4da96746bf3e4e50504b9516
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 00:23:45 -0400

    Saving plot.R

--- plot.R
+++ plot.R
@@ -2,6 +2,7 @@
 
 
 install.packages("ggmap")
+install.packages("ggplot")
 usa <- map_data("state")
 census_arrests1$stateName <- tolower(census_arrests1$stateName)
  mapsimple <- ggplot(census_arrests1, aes(map_id = stateName))   
