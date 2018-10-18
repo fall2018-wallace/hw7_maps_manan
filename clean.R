8ef45f4aa7529693e11e9c4f7091d4e98cc6d0b9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:28:58 -0400

    removing clean.R

--- clean.R
+++
@@ -1,9 +0,0 @@
-
-dfStates <- read.csv("scprc_est_2017_18_pop_res.csv") 
-  dfStates <- dfStates[-1,]
-  dfStates <- dfStates[-52,]
-  dfStates <- dfStates[-9,]
-  dfStates <- dfStates[,-1:-4]
-  colnames(dfStates) <- c("stateName","population","popOver18","percentOver18")
-  dfStates
-  
